cf0547232e41c2bc94f1c2dfe0cdc8448d146008
qtcoder999 <30284500+qtcoder999@users.noreply.github.com>
2021-03-09 17:15:16 +0530

    Update wrapper.sh

--- wrapper.sh
+++ wrapper.sh
@@ -1,1 +1,14 @@
-sudo sh /home/paras/custom-scripts/test.sh | tee -a /home/paras/custom-scripts/log.txt
+#!/bin/sh
+
+LOG_FILE_PATH="/home/paras/custom-scripts/log.txt"
+SCRIPT_FILE_PATH="/home/paras/custom-scripts/trigger-upgrade.sh"
+MAX_LINE_COUNT=1000
+
+
+LOG_FILE_LINE_COUNT=`wc -l < $LOG_FILE_PATH`
+
+if [ $LOG_FILE_LINE_COUNT -gt $MAX_LINE_COUNT ]; then
+    echo -n >$LOG_FILE_PATH
+fi
+
+sudo sh $SCRIPT_FILE_PATH | tee -a $LOG_FILE_PATH
